9138873062e906db71b90040d5910b31357c4012
Gwoop <gwooparap@gmail.com>
2022-09-03 22:41:51 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -0,0 +1,58 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("name","ну допустим это просто");
+        return "index";
+    }
+
+    @GetMapping("/calc")
+    public String calc(
+            @RequestParam(
+                    value = "num_1",
+                    required = false,
+            defaultValue = "1") double first,
+            @RequestParam(
+                    value = "num_2",
+                    required = false,
+                    defaultValue = "1") double second,
+            @RequestParam(
+                    value = "objectiv",
+                    required = false,
+                    defaultValue = "1") String obj,
+            Model model) {
+
+        double res = 0;
+        //String test = first + second + obj;
+
+        switch (obj){
+            case ("/"):
+                res = first/second;
+                break;
+            case ("*"):
+                res = first*second;
+                break;
+            case ("+"):
+                res = first+second;
+                break;
+            case ("-"):
+                res = first-second;
+                break;
+        }
+        model.addAttribute("result",res);
+
+        return "kalkul";
+    }
+
+
+
+
+
+}
