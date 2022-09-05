5bd2c09276da73d6ebff2dc2d618bf32b82ffcc7
Gwoop <gwooparap@gmail.com>
2022-09-05 09:17:29 +0300

    apdate

--- src/main/java/com/example/demo/controllers/FirstController.java
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -12,6 +12,48 @@ public class FirstController {
         model.addAttribute("name","ну допустим это просто");
         return "index";
     }
+    @GetMapping("/calcpost")
+    public String calcpost(Model model) {
+        model.addAttribute("name","ну допустим это просто");
+        return "KalkulPost";
+    }
+
+
+    @PostMapping("/calcpost")
+    public String calcpost(
+            @RequestParam(
+                    value = "num_1",
+                    required = false,
+                    defaultValue = "1") double first,
+            @RequestParam(
+                    value = "num_2",
+                    required = false,
+                    defaultValue = "1") double second,
+            @RequestParam(
+                    value = "objectiv",
+                    required = false,
+                    defaultValue = "1") String obj,
+            Model model) {
+        double res = 0;
+        //String test = first + second + obj;
+        switch (obj){
+            case ("/"):
+                res = first/second;
+                break;
+            case ("*"):
+                res = first*second;
+                break;
+            case ("+"):
+                res = first+second;
+                break;
+            case ("-"):
+                res = first-second;
+                break;
+        }
+        model.addAttribute("res",res);
+
+        return "res";
+    }
 
     @GetMapping("/calc")
     public String calc(
